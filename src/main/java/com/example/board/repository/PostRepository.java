e9b811cfc0cf4c868ab372eea49b105fb4d10a65
Saiki <raypipi1104@yahoo.co.jp>
2022-07-02 10:15:43 +0900

    first commit

---
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -0,0 +1,38 @@
+package com.example.board.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/**
+ * 投稿のリポジトリー.
+ */
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+/**
+ * IDで検索する
+ * 
+ * @param id ID
+ * return 投稿
+ */
+public Optional<Post> findById(String id);
+
+/**
+ * 更新日時の降順で全ての投稿を検索する
+ * 
+ * 　@return 投稿のリスト
+ */
+List<Post> findAllByOrderByUpdatedDateDesc();
+
+/**
+ * 更新日時の降順で未削除の投稿を検索する
+ * 
+ * @return 投稿のリスト
+ */
+List<Post> findByDeletedFalseOrderByUpdatedDateDesc();
+
+}
+
+
